40b675f9e7f0916a7db785f7f13d540dde938d73
Nathan Woodhull <woodhull@gmail.com>
2019-09-05 18:15:20 -0400

    Add CloudWatch log group to Terraform so we can set an expiration policy.

---
+++ logs.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudwatch_log_group" "loader" {
+  name = "/aws/lambda/controlshift-redshift-loader"
+  retention_in_days = 5
+
+  tags = {
+    Application = "controlshift-redshift"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "webhook" {
+  name = "/aws/lambda/recieve-webhook-handler"
+  retention_in_days = 5
+  tags = {
+    Application = "controlshift-redshift"
+  }
+}
