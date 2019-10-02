9675faf69dc7481adb23b83b4c16b74b39005ab2
Ankur <ankur.naik@moveon.org>
2019-10-02 13:33:32 -0700

    Add support for sending SNS notifications during batch processing (#1)
    * Adding support for sending SNS notifications for batch processing successes and failures. The SNS topics will be created by Terraform, after which subscriptions (e.g., email addresses) can be added to each topic via the SNS console in AWS to specify where the notifications should be delivered.
    
    The awslabs/aws-lambda-redshift-loader repository contains a Lambda function that can be used to automatically reprocess failed batches by subscribing the Lambda to the failure SNS topic, but I haven't tested that yet.
    
    * Use aws_iam_policy_document to describe IAM policies.
    
    * Just use one IAM policy with two resources.

--- loader.tf
+++ loader.tf
@@ -36,3 +36,37 @@ resource "aws_s3_bucket_notification" "notifications" {
     filter_prefix       = "full"
   }
 }
+
+resource "aws_sns_topic" "success_sns_topic" {
+  name = var.success_topic_name
+  policy = data.aws_iam_policy_document.sns_notification_policy.json
+}
+
+resource "aws_sns_topic" "failure_sns_topic" {
+  name = var.failure_topic_name
+  policy = data.aws_iam_policy_document.sns_notification_policy.json
+}
+
+data "aws_iam_policy_document" "sns_notification_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "SNS:Publish",
+    ]
+    resources = [
+      "arn:aws:sns:*:*:${var.success_topic_name}",
+      "arn:aws:sns:*:*:${var.failure_topic_name}",
+    ]
+    condition {
+      test      = "ArnLike"
+      variable  = "aws:SourceArn"
+      values    = [
+        aws_lambda_function.loader.arn,
+      ]
+    }
+  }
+}
