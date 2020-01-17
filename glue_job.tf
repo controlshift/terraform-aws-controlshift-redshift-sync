17000cffa16047160e4e269c3a2c267e1a960bde
Grey Moore <grey@controlshiftlabs.com>
2020-01-17 15:21:02 -0500

    create the iam role for the glue jobs

--- glue_job.tf
+++ glue_job.tf
@@ -16,11 +16,30 @@ resource "aws_s3_bucket_object" "signatures_script" {
   content = data.template_file.signatures_script.rendered
 }
 
+resource "aws_iam_role" "glue_service_role" {
+  name = "AWSGlueServiceRole"
+  description = "Used by the AWS Glue jobs to insert data into redshift"
+  assume_role_policy = data.aws_iam_policy_document.glue_assume_role.json
+}
+
+data "aws_iam_policy_document" "glue_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["glue.amazonaws.com"]
+    }
+  }
+}
+
+# TODO: give glue_service_role some permissions as currently held by
+#       AWSGlueServiceRole-ManualTest
+
 resource "aws_glue_job" "signatures_full" {
   name = "cs-${var.controlshift_environment}-signatures-full"
 
-  # TODO
-  role_arn = "arn:aws:iam::087959666724:role/service-role/AWSGlueServiceRole-ManualTest"
+  role_arn = aws_iam_role.glue_service_role.arn
 
   command {
     script_location = "s3://${aws_s3_bucket.glue_script.bucket}/${var.controlshift_environment}/signatures_job.py"
