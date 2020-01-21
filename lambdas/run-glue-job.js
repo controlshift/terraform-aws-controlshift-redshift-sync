7bf911bfa1af032bd5ba4d4f9dfb2aba02228946
Nathan Woodhull <woodhull@gmail.com>
2020-01-21 18:02:51 -0500

    Work in progress: a lambda function for triggering glue jobs

---
+++ lambdas/run-glue-job.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: process.env.AWS_REGION});
+
+// Create an SQS service object
+const sqs = new AWS.SQS();
+const glue = new AWS.Glue();
+const jobName = 'glue-job-name';
+
+exports.handler = async function(event, context) {
+  event.Records.forEach(record => {
+    const { body } = record;
+    console.log(body);
+
+    glue.startJobRun({JobName: jobName}, function(error, data) {
+      if(error) {
+        console.log(error, error.stack);
+      } else {
+        console.log(data);
+      }
+    });
+
+  });
+  return {};
+};
