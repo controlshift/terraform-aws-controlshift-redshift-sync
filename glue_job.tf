0408c257a15f23a864d2aeb0280899a29cbe6f16
anero <anero79@gmail.com>
2020-01-22 18:17:51 -0300

    Glue job resource expects connection to be referenced by name. Also added default parameters for job

--- glue_job.tf
+++ glue_job.tf
@@ -112,9 +112,13 @@ resource "aws_glue_connection" "redshift_connection" {
 
 resource "aws_glue_job" "signatures_full" {
   name = "cs-${var.controlshift_environment}-signatures-full"
-  connections = [ aws_glue_connection.redshift_connection.id ]
+  connections = [ aws_glue_connection.redshift_connection.name ]
   glue_version = "1.0"
-  default_arguments = { "--TempDir": "s3://${aws_s3_bucket.glue_resources.bucket}/${var.controlshift_environment}/temp" }
+  default_arguments = {
+    "--TempDir": "s3://${aws_s3_bucket.glue_resources.bucket}/${var.controlshift_environment}/temp",
+    "--job-bookmark-option": "job-bookmark-disable",
+    "--job-language": "python"
+  }
 
   role_arn = aws_iam_role.glue_service_role.arn
 
