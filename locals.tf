7f083173cb4ddc8b7428b006185baed5ba0a5261
anero <anero79@gmail.com>
2020-01-29 15:56:09 -0300

    Comment explaining support for additional regions for Lambda functions storage can be requested

--- locals.tf
+++ locals.tf
@@ -1,3 +1,5 @@
+// Additional regions can be added. Please contact support@controlshiftlabs.com
+// if you are operating in a region other than these.
 locals {
   lambda_buckets = {
     "us-east-1" = "changesprout-lambdas-us-east-1"
