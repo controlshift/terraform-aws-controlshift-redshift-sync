2ea5dde7ee29a73e12d3482b5e139338947460dd
Grey Moore <grey@controlshiftlabs.com>
2021-11-01 10:57:14 -0400

    Update syntax to work with modern Terraform (#26)
    * provider blocks and versions
    
    * skip the weird adding an empty list and then compacting, just use the variables directly
    
    * require terraform version 0.13 or greater

---
+++ versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    archive = {
+      source = "hashicorp/archive"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 2.0"
+    }
+    http = {
+      source = "hashicorp/http"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+}
