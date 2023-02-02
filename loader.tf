33ed1bb18c9584d815cc8dd740479081b605a93a
Grey Moore <grey@controlshiftlabs.com>
2023-02-02 10:25:04 -0500

    use AwsLambdaRedshiftLoader version 2.8.3
    this version has updated dependencies to address security fixes

--- loader.tf
+++ loader.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "loader" {
   s3_bucket = local.lambda_buckets[var.aws_region]
-  s3_key = "LambdaRedshiftLoader/AWSLambdaRedshiftLoader-2.8.2.zip"
+  s3_key = "LambdaRedshiftLoader/AWSLambdaRedshiftLoader-2.8.3.zip"
   function_name = "controlshift-redshift-loader"
   role          = aws_iam_role.loader_lambda_role.arn
   handler       = "index.handler"
